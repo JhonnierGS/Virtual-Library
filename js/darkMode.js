dc2a7aa56608d71cc1b01fbc94f28bcdf3293438
JhonnierGS <gs.170590@gmail.com>
2021-03-25 16:41:45 -0500

    arreglando logica del darkmode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,5 +1,6 @@
 const btnDark = document.querySelector('.btn-dark-mode');
 
+
 btnDark.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 
@@ -14,4 +15,24 @@ btnDark.addEventListener('click', () => {
         Dark Mode
         `
     }
+
+    if(document.body.classList.contains('dark-mode')){
+        localStorage.setItem('dark', 'true')
+    }else{
+        localStorage.setItem('dark', 'false')
+    }
 })
+
+if(localStorage.getItem('dark') === 'true'){
+    document.body.classList.add('dark-mode');
+    btnDark.innerHTML = `
+        <i class="far fa-sun"></i>
+        Light Mode
+        `
+}else{
+    document.body.classList.remove('dark-mode');
+    btnDark.innerHTML = `
+        <i class="far fa-moon"></i>
+        Dark Mode
+        `
+}
